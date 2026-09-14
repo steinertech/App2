5e7873ecd2421de0157e3b519b1091ab98ce4ad5
User <205841367+steinertech@users.noreply.github.com>
2026-09-14 19:02:14 +0200

    Plane

--- App.Server/util/util-main.ts
+++ App.Server/util/util-main.ts
@@ -1,7 +1,7 @@
 import { userSession } from './util-user.js';
 import { GridStateDto } from '../dto/web/grid-dto.js';
 
-export const VERSION_SERVER = '1.15';
+export const VERSION_SERVER = '1.16';
 
 export function domainName(request: Request): string {
   const originHeader = request.headers.get('origin');
